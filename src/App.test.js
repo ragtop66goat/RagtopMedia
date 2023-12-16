2a2124357132aa52cce73b39dcc6a44319ee1fbf
Jason Gionfriddo <Ragtop66goat@gmail.com>
2023-12-15 23:03:12 -0600

    added workflow to deploy to netlify with secrets

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,20 @@
 import {render, screen} from '@testing-library/react';
 import App from './App';
+
 import '@testing-library/jest-dom/extend-expect';
 
-  jest.mock('./pages/work/Work', (() => {
+  jest.mock('./pages/home/Home', (() => {
     return () => <div>Home</div>;
   }));
+
 
 describe("App tests", () => {
 
 
   it('should render the home page', () => {
-    render(<App/>);
+    render(
+        <App/>
+    )
 
     expect(screen.getByText("Home")).toBeInTheDocument();
   })
